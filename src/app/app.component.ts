4d2c93637566c2943bb45b37693573722eaabc15
Hetdev <hetzel30@gmail.com>
2019-08-13 13:58:07 -0500

    equal listener

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'web-comp-ng-element POC';
+
+  @Output() changeNameToEventEmit = new EventEmitter<any>();
+
+  changeNameToHetdev() {
+    const data = {
+      name: 'Hetdev'
+    };
+    const event = new CustomEvent('changeNameToCustomEvent', { detail: data });
+    window.dispatchEvent(event);
+    this.changeNameToEventEmit.emit(data);
+
+  }
 }
